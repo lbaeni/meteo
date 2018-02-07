d765132708b1d000810dc4dcfce2430e37b533b8
lbaeni <lbaeni@users.github.com>
2018-02-07 16:52:13 +0100

    access database within write_json function

--- python/write_meteoJSON.py
+++ python/write_meteoJSON.py
@@ -4,11 +4,17 @@ import os, sys
 import meteo_data
 
 
-def write_json(output_path, module, correction) :
+def write_json(db_path, output_path, module, correction) :
+	'''read meteo data from database and write it to a json file'''
+
+	# read meteo data from database
+	db = meteo_data.meteo_data(db_path)
 	data = db.get_data(module, 2678400) # get last 31 days
 #	data = db.get_data(module, 86400.*60) # get last 60 days
 #	data = db.get_data(module, 86400.*100) # get last 100 days
 #	data = db.get_data(module) # get all data
+	db.close()
+
 	# writing meteo data to file
 	json = open(output_path+'/'+module+'.json', 'w')
 	json.write('[')
@@ -23,10 +29,5 @@ def write_json(output_path, module, correction) :
 
 db_path = sys.argv[1]
 output_path = sys.argv[2]
-global db
-db = meteo_data.meteo_data(db_path)
-write_json(output_path, 'SENSORSERIALNUMBER1', [0., 0., 0., 0., 0.])
-write_json(output_path, 'SENSORSERIALNUMBER2', [0., 0., 0., 0., 0.])
-
-
-db.close()
+write_json(db_path, output_path, 'SENSORSERIALNUMBER1', [0., 0., 0., 0., 0.])
+write_json(db_path, output_path, 'SENSORSERIALNUMBER2', [0., 0., 0., 0., 0.])
