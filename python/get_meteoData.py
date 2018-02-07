12960ae047d30f9a14ca28359697f6adb7526bd7
lbaeni <lbaeni@users.github.com>
2018-02-07 16:41:15 +0100

    add class to read out meteo sensors

--- python/get_meteoData.py
+++ python/get_meteoData.py
@@ -1,47 +1,9 @@
 #!/usr/bin/python
-import sqlite3
-import time
 import os, sys
-import meteo_data
-# add /usr/lib/yoctopuce to the PYTHONPATH
-sys.path.append(os.path.join("/usr/lib/yoctopuce"))
-from yocto_api import *
-from yocto_humidity import *
-from yocto_temperature import *
-from yocto_pressure import *
+import yocto_meteo
 
-
-def die(msg):
-	sys.exit(msg+' (check USB cable)')
-
-def init_module(target) :
-	errmsg=YRefParam()
-	# Setup the API to use local USB devices
-	if YAPI.RegisterHub("usb", errmsg)!= YAPI.SUCCESS:
-		sys.exit("init error"+errmsg.value)
-	global module
-	module = YModule.FindModule(target)
-	if not module.isOnline() : die('device not connected')
-	global hum_sensor, temp_sensor, press_sensor
-	hum_sensor   = YHumidity   .FindHumidity   (target+'.humidity')
-	press_sensor = YPressure   .FindPressure   (target+'.pressure')
-	temp_sensor  = YTemperature.FindTemperature(target+'.temperature')
-
-def write_currentData() :
-	timestamp = time.time()
-	temp  = temp_sensor .get_currentValue()
-	hum   = hum_sensor  .get_currentValue()
-	press = press_sensor.get_currentValue()
-	module_name = module.get_serialNumber()
-	db.add_data(module_name, timestamp, temp, hum, press)
 
 target = sys.argv[1]
 db_path = sys.argv[2]
-global db
-db = meteo_data.meteo_data(db_path)
-init_module(target)
-write_currentData()
-
-
-
-db.close()
+sensor = yocto_meteo.yocto_meteo(target)
+sensor.write_currentData(db_path)
---
+++ python/yocto_meteo.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import time
+import os, sys
+import meteo_data
+# add /usr/lib/yoctopuce to the PYTHONPATH
+sys.path.append(os.path.join("/usr/lib/yoctopuce"))
+from yocto_api import *
+from yocto_humidity import *
+from yocto_temperature import *
+from yocto_pressure import *
+
+
+class yocto_meteo(object) :
+
+	def __init__(self, target) :
+		errmsg = YRefParam()
+
+		# Setup the API to use local USB devices
+		if YAPI.RegisterHub("usb", errmsg)!= YAPI.SUCCESS:
+			sys.exit("init error"+errmsg.value)
+		self.module = YModule.FindModule(target)
+		if not self.module.isOnline() : self.die('device not connected')
+		self.hum_sensor   = YHumidity   .FindHumidity   (target+'.humidity')
+		self.press_sensor = YPressure   .FindPressure   (target+'.pressure')
+		self.temp_sensor  = YTemperature.FindTemperature(target+'.temperature')
+
+
+	def die(self, msg) :
+		sys.exit(msg+' (check USB cable)')
+
+
+	def write_currentData(self, db_path) :
+		timestamp = time.time()
+		temp  = self.temp_sensor .get_currentValue()
+		hum   = self.hum_sensor  .get_currentValue()
+		press = self.press_sensor.get_currentValue()
+		module_name = self.module.get_serialNumber()
+		db = meteo_data.meteo_data(db_path)
+		db.add_data(module_name, timestamp, temp, hum, press)
+		db.close()
