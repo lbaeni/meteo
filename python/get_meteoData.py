5a3728ca5f702691ca223e2a4911b06378c02396
lbaeni <lbaeni@users.github.com>
2018-02-07 21:18:19 +0100

    read out all meteo sensors defined in config file

--- python/get_meteoData.py
+++ python/get_meteoData.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
 import os, sys
 import yocto_meteo
+import ConfigParser
 
 
-target = sys.argv[1]
-db_path = sys.argv[2]
-sensor = yocto_meteo.yocto_meteo(target)
-sensor.write_currentData(db_path)
+db_path = os.path.dirname(os.path.realpath(__file__)) + '/../test'
+config_file = os.path.dirname(os.path.realpath(__file__)) + '/../modules.cfg'
+config = ConfigParser.ConfigParser()
+config.optionxform = str # case sensitive options
+config.read(config_file)
+print config.sections()
+
+for sensor in config.sections() :
+	if config.get(sensor, 'type') != 'meteo' : continue
+	target = config.get(sensor, 'serial_number')
+	sensor = yocto_meteo.yocto_meteo(target)
+	sensor.write_currentData(db_path)
