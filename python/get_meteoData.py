5cf66684796b1e77680d1c43c777c5add902eea3
lbaeni <lbaeni@users.github.com>
2018-02-10 18:11:52 +0100

    compare temperature of sensors

--- python/get_meteoData.py
+++ python/get_meteoData.py
@@ -20,16 +20,22 @@ config.read(config_file)
 meteo_sensors = [module for module in config.sections() if config.get(module, 'type') == 'meteo' ]
 buzzers       = [module for module in config.sections() if config.get(module, 'type') == 'buzzer']
 
+data = {}
 for module in meteo_sensors :
+	data[module] = {}
 	target = config.get(module, 'serial_number')
 	sensor = yocto_meteo.yocto_meteo(target)
-	[temp, hum, press] = sensor.write_currentData(db_path)
+	[data[module]['temp'], data[module]['hum'], data[module]['press']] = sensor.write_currentData(db_path)
+	sensor.turn_beaconOff()
+	print data[module]['temp']
+
+	# activate buzzer if temperature is above threshold
 	if config.has_option(module, 'buzzer') :
 		buz_name   = config.get(module  , 'buzzer'       )
 		buz_target = config.get(buz_name, 'serial_number')
 		buz = buzzer.buzzer(buz_target)
 		temp_threshold = float(config.get(module, 'temp_threshold'))
-		if temp > temp_threshold :
+		if data[module]['temp'] > temp_threshold :
 			if not buz.get_ledPower(2) :
 				buz.play_alarm()
 			buz.turn_ledOff(1)
@@ -37,3 +43,13 @@ for module in meteo_sensors :
 		else :
 			buz.turn_ledOff(2)
 			buz.flash_led(1, 'STILL', 1)
+
+# compare temperature of different sensors
+for module in meteo_sensors :
+	if not config.has_option(module, 'temp_threshold') : continue
+	reference_module = config.get(module, 'temp_threshold')
+	if not reference_module in meteo_sensors : continue
+	target = config.get(module, 'serial_number')
+	sensor = yocto_meteo.yocto_meteo(target)
+	if data[module]['temp'] > data[reference_module]['temp'] : sensor.turn_beaconOn()
+	else                                                     : sensor.turn_beaconOff()
