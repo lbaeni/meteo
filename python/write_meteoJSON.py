5ffe9a98b2c1771018227405dd47dfa2a32055ae
lbaeni <lbaeni@users.github.com>
2018-02-24 11:59:52 +0100

    use with statement when writing json file

--- python/write_meteoJSON.py
+++ python/write_meteoJSON.py
@@ -15,23 +15,22 @@ def write_json(db_path, output_path, module, correction, period = -1) :
 	db.close()
 
 	# writing meteo data to file
-	json = open(output_path+'/'+module+'.json', 'w')
-	json.write('[')
-	i = 0
-	last_timestamp = data[0][1]
-	for value in data :
-		if abs(value[1] - last_timestamp) > 90. :
-			j = 0
-			while j < int((abs(value[1] - last_timestamp) - 30.) / 60.) :
-				json.write('[{0}, null, null, null],\n'.format(last_timestamp + (j+1)*60.))
-				j += 1
-		json.write('[{0},{1},{2},{3}]'.format(value[1], value[2]+correction[2], value[3]+correction[3], value[4]+correction[4]))
-		i = i+1
-		if i < len(data) :
-			json.write(',\n')
-		last_timestamp = value[1]
-	json.write(']')
-	json.close()
+	with open(output_path+'/'+module+'.json', 'w') as json :
+		json.write('[')
+		i = 0
+		last_timestamp = data[0][1]
+		for value in data :
+			if abs(value[1] - last_timestamp) > 90. :
+				j = 0
+				while j < int((abs(value[1] - last_timestamp) - 30.) / 60.) :
+					json.write('[{0}, null, null, null],\n'.format(last_timestamp + (j+1)*60.))
+					j += 1
+			json.write('[{0},{1},{2},{3}]'.format(value[1], value[2]+correction[2], value[3]+correction[3], value[4]+correction[4]))
+			i = i+1
+			if i < len(data) :
+				json.write(',\n')
+			last_timestamp = value[1]
+		json.write(']')
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('-d', '--database', help = 'path of meteo database', default = os.path.dirname(os.path.realpath(__file__)) + '/../test/meteo.db')
